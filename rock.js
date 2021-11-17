a6d55a02efa2ce1889242cfe65c2131b4a8e1a84
DJacobson17 <davidbjacobson@gmail.com>
2021-11-17 10:16:23 -0600

    created player selection and playRound function

--- rock.js
+++ rock.js
@@ -8,7 +8,29 @@ function computerPlay() {
     return num === 0 ? "Rock"
          : num === 1  ? "Paper"
          : "Scissors";
-    
+
 
 }
-console.log (computerPlay(getRandomArbitrary(max)))
+let computerSelection = computerPlay(getRandomArbitrary(max))
+//Input player selection;
+player = prompt ("Choose rock, paper, or scissors");
+//make case insensitive;
+const playerSelection = player[0].toUpperCase() + player.substring(1).toLowerCase();
+console.log(playerSelection);
+//play round;
+function playRound(playerSelection, computerSelection)  {
+
+//compare computer selection to player selection;
+ 
+//test all possible outcomes;
+return playerSelection == computerSelection ? "It's a Tie!"
+    : playerSelection == "Rock" && computerSelection == "Paper" ? "You Lose! " + computerSelection + " beats " + playerSelection
+    : playerSelection == "Rock" && computerSelection == "Scissors" ? "You Win! " + playerSelection + " beats " + computerSelection
+    : playerSelection == "Paper" && computerSelection == "Rock" ? "You Win! " + playerSelection + " beats " + computerSelection
+    : playerSelection == "Paper" && computerSelection == "Scissors" ? "You Lose! " + computerSelection + " beats " + playerSelection
+    : playerSelection == "Scissors" && computerSelection == "Rock" ? "You Lose! " + computerSelection + " beats " + playerSelection
+    : "You Win! " + playerSelection + " beats " + computerSelection;
+}
+//output string declaring results;
+
+console.log (playRound(playerSelection, computerSelection));
