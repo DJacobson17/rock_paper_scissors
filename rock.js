8a5a0395cee2d627ea914b21845adec87530210e
DJacobson17 <davidbjacobson@gmail.com>
2021-11-19 12:34:35 -0600

    put player selection and computer selection inside playRound function and added game function

--- rock.js
+++ rock.js
@@ -1,36 +1,57 @@
-max = 3;
 function getRandomArbitrary() {
+    let max = 3;
     return Math.floor(Math.random() * max);
 }
 
 function computerPlay() {
-    num = getRandomArbitrary(max)
+    num = getRandomArbitrary()
     return num === 0 ? "Rock"
          : num === 1  ? "Paper"
          : "Scissors";
 
 
 }
-let computerSelection = computerPlay(getRandomArbitrary(max))
+function computerSelection() {
+    return (computerPlay(getRandomArbitrary()))
+}
 //Input player selection;
-player = prompt ("Choose rock, paper, or scissors");
+function playerSelection() {
+    let player = prompt ("Choose rock, paper, or scissors");
 //make case insensitive;
-const playerSelection = player[0].toUpperCase() + player.substring(1).toLowerCase();
-console.log(playerSelection);
-//play round;
-function playRound(playerSelection, computerSelection)  {
+    return (player[0].toUpperCase() + player.substring(1).toLowerCase());
+}
 
+//play round;
+function playRound()  {
+    let pS = playerSelection();
+    let cS = computerSelection();
 //compare computer selection to player selection;
  
 //test all possible outcomes;
-return playerSelection == computerSelection ? "It's a Tie!"
-    : playerSelection == "Rock" && computerSelection == "Paper" ? "You Lose! " + computerSelection + " beats " + playerSelection
-    : playerSelection == "Rock" && computerSelection == "Scissors" ? "You Win! " + playerSelection + " beats " + computerSelection
-    : playerSelection == "Paper" && computerSelection == "Rock" ? "You Win! " + playerSelection + " beats " + computerSelection
-    : playerSelection == "Paper" && computerSelection == "Scissors" ? "You Lose! " + computerSelection + " beats " + playerSelection
-    : playerSelection == "Scissors" && computerSelection == "Rock" ? "You Lose! " + computerSelection + " beats " + playerSelection
-    : "You Win! " + playerSelection + " beats " + computerSelection;
+    return pS == cS ? "It's a Tie!"
+        : pS == "Rock" && cS == "Paper" ? "You Lose! " + cS + " beats " + pS
+        : pS == "Rock" && cS == "Scissors" ? "You Win! " + pS + " beats " + cS
+        : pS == "Paper" && cS == "Rock" ? "You Win! " + pS + " beats " + cS
+        : pS == "Paper" && cS == "Scissors" ? "You Lose! " + cS + " beats " + pS
+        : pS == "Scissors" && cS == "Rock" ? "You Lose! " + cS + " beats " + pS
+        : "You Win! " + pS + " beats " + cS;
 }
 //output string declaring results;
-
-console.log (playRound(playerSelection, computerSelection));
+//console.log (playRound())
+//repeat 5 times;
+function game() {
+    let c = 0;
+    let pScore = 0;
+    let cScore = 0;
+    let tie = 0;
+    while (c < 5) {
+        let pR = playRound()
+        c++;
+        pR.includes("Win") ? pScore += 1
+            : pR.includes("Lose") ? cScore += 1
+            : tie += 1;
+        console.log (pR);
+    }
+    return ("Your score: " + pScore + " My score: " + cScore)     
+}
+console.log(game());
